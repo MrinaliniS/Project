a2940fbfa293e2cf3cecec76f267a560be390903
MrinaliniS <shirimrinalini@gmail.com>
2017-12-01 13:41:07 +0530

    Update run_hbase.sh

--- proj2/run_hbase.sh
+++ proj2/run_hbase.sh
@@ -6,6 +6,6 @@ hadoop fs -copyFromLocal /home/hduser1/646f8e796f044f1d8e459f2d28f4caab /tmp/tim
 
 HADOOP_CLASSPATH=`/usr/hdp/2.4.2.0-258/hbase/bin/hbase classpath` /usr/hdp/2.4.2.0-258/hadoop/bin/hadoop jar /usr/hdp/2.4.2.0-258/hbase/lib/hbase-server-1.1.2.2.4.2.0-258.jar completebulkload hdfs://mdw.lss.emc.com:8020/tmp/tim_data/  tim_ericcson_bulk
 
-spark-submit  --packages com.hortonworks:shc-core:1.1.1-2.1-s_2.11 --repositories http://repo.hortonworks.com/content/groups/public/ run.py
+spark-submit  --packages com.hortonworks:shc:1.0.0-1.6-s_2.10 --repositories http://repo.hortonworks.com/content/groups/public/ run.py
 
 hbase shell /home/hduser1/list.txt
